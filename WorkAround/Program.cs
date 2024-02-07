7748c6edef8adfeb4037dc189b73a5b863c1151a
kamilbahram <kamil.bahram@bil.omu.edu.tr>
2024-02-07 22:20:17 +0300

    KPSPublic servisinden kimlik sorgulama uygulaması gerçekleştirilmiştir.

---
+++ WorkAround/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Business.Conrete;
+using Entities.Concrete;
+
+Console.WriteLine("Hello, World!");
+
+Person person = new Person();
+person.NationalIdentity = 32903294866;
+person.FirstName = "Kamil";
+person.LastName = "Bahram";
+person.DateOfBirthYear = 1995;
+
+PttManager pttManager = new PttManager(new PersonManager());
+pttManager.GiveMask(person);
